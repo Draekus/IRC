a487cec328d3351785b0f3f72f3013fbf2ae1eac
pgould94 <patrick.gould94@gmail.com>
2019-10-15 01:01:44 -0400

    added irc server files

---
+++ server.js
@@ -0,0 +1,39 @@
+// Dependencies
+// =============================================================
+// Web Server Imports
+const express = require("express");
+const path = require("path");
+
+// IRC Server Imports
+const IRC = require('irc-server');
+
+// Sets up the Express App
+// =============================================================
+// const app = express();
+const PORT = process.env.PORT;
+
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "view.html"));
+// });
+
+// Sets up the IRC Server
+// =============================================================
+const server = IRC.createServer();
+
+// Starts the web server to begin listening
+// =============================================================
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
+// });
+
+// Starts the IRC server to begin listening
+// =============================================================
+server.listen(PORT);
